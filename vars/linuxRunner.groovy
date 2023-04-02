07e53431d4e986bc730bdf43693c6f47b55397e7
Anat Shulman <sanatash@gmail.com>
2023-04-02 19:47:56 +0300

    shared library functions will receive container image version as argument

--- vars/linuxRunner.groovy
+++ vars/linuxRunner.groovy
@@ -1,6 +1,6 @@
-def call(String podLabel, code) {
-	podTemplate(label: podLabel, containers: [containerTemplate(name: 'centos', image: 'centos:7', ttyEnabled: true),
-	                                     containerTemplate(name: 'ubuntu', image: 'ubuntu:latest', ttyEnabled: true)]) {
+def call(String podLabel, String centosVersion, String ubuntuVersion, code) {
+	podTemplate(label: podLabel, containers: [containerTemplate(name: 'centos', image: 'centos:${centosVersion}', ttyEnabled: true),
+	                                     containerTemplate(name: 'ubuntu', image: 'ubuntu:${ubuntuVersion}', ttyEnabled: true)]) {
 		code()
 	}
 }
--- vars/mavenRunner.groovy
+++ vars/mavenRunner.groovy
@@ -1,6 +1,6 @@
-def call(String podLabel, code) {
+def call(String podLabel, String mavenVersion, code) {
 	podTemplate(label: podLabel,
-	containers: [containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true)
+	containers: [containerTemplate(name: 'maven', image: 'maven:${mavenVersion}', ttyEnabled: true)
 	       ]) {
 		code()
 	}
