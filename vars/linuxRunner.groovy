b270b4f4e5f932f58b5dae897384eba4c3ddca73
Anat Shulman <sanatash@gmail.com>
2023-04-02 16:19:42 +0300

    adds shared library jenkins functions

---
+++ vars/linuxRunner.groovy
@@ -0,0 +1,6 @@
+def call(String podLabel, code) {
+	podTemplate(label: podLabel, containers: [containerTemplate(name: 'centos', image: 'centos:7', command: ['cat'], ttyEnabled: true),
+	                                     containerTemplate(name: 'ubuntu', image: 'ubuntu:latest', command: ['cat'], ttyEnabled: true)]) {
+		code()
+	}
+}
---
+++ vars/mavenRunner.groovy
@@ -0,0 +1,7 @@
+def call(String podLabel, code) {
+	podTemplate(label: podLabel,
+	containers: [containerTemplate(name: 'maven', image: 'image: maven:3.3.9-jdk-8-alpine', command: ['cat'], ttyEnabled: true)
+	       ]) {
+		code()
+	}
+}
