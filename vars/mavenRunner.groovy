091c20b5e07dc2b66059d6eeaec187af3198135d
Anat Shulman <sanatash@gmail.com>
2023-04-02 19:23:01 +0300

    another argument fix in maven shared library function

--- vars/mavenRunner.groovy
+++ vars/mavenRunner.groovy
@@ -1,6 +1,6 @@
 def call(String podLabel, code) {
 	podTemplate(label: podLabel,
-	containers: [containerTemplate(name: 'maven', image: 'image: maven:latest', ttyEnabled: true)
+	containers: [containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true)
 	       ]) {
 		code()
 	}
