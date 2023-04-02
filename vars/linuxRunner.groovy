380d1e2ef21a7f24543065e9b0efa6622832ee45
Anat Shulman <sanatash@gmail.com>
2023-04-02 19:56:10 +0300

    Use double quotes around image name

--- vars/linuxRunner.groovy
+++ vars/linuxRunner.groovy
@@ -1,6 +1,6 @@
 def call(String podLabel, String centosVersion, String ubuntuVersion, code) {
-	podTemplate(label: podLabel, containers: [containerTemplate(name: 'centos', image: 'centos:${centosVersion}', ttyEnabled: true),
-	                                     containerTemplate(name: 'ubuntu', image: 'ubuntu:${ubuntuVersion}', ttyEnabled: true)]) {
+	podTemplate(label: podLabel, containers: [containerTemplate(name: 'centos', image: "centos:${centosVersion}", ttyEnabled: true),
+	                                     containerTemplate(name: 'ubuntu', image: "ubuntu:${ubuntuVersion}", ttyEnabled: true)]) {
 		code()
 	}
 }
--- vars/mavenRunner.groovy
+++ vars/mavenRunner.groovy
@@ -1,6 +1,6 @@
 def call(String podLabel, String mavenVersion, code) {
 	podTemplate(label: podLabel,
-	containers: [containerTemplate(name: 'maven', image: 'maven:${mavenVersion}', ttyEnabled: true)
+	containers: [containerTemplate(name: 'maven', image: "maven:${mavenVersion}", ttyEnabled: true)
 	       ]) {
 		code()
 	}
